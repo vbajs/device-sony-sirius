e1d33c81255e2d494ebc998fc69918869e797012
Yahya Wessam <vbajs@users.noreply.github.com>
2016-05-25 01:32:12 +0300

    Update and rename aosp_d6503.mk to full_d6503.mk

--- full_d6503.mk
+++ full_d6503.mk
@@ -19,9 +19,9 @@ $(call inherit-product, device/sony/sirius/device.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-PRODUCT_NAME := aosp_d6503
+PRODUCT_NAME := full_d6503
 PRODUCT_DEVICE := sirius
-PRODUCT_MODEL := Xperia Z2 (AOSP)
+PRODUCT_MODEL := Xperia Z2 
 PRODUCT_BRAND := Sony
 PRODUCT_MANUFACTURER := Sony
 
