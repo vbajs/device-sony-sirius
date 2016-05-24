ec2fe4a8c9cc0efc8d9310ec263942be2e3f8776
Yahya Wessam <vbajs@users.noreply.github.com>
2016-05-25 01:35:27 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/aosp_d6503.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/turbo.mk
 
